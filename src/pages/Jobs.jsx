9704dd637d9f96ea1ce1d173c8777218737563f9
Alexandre Ferreira <alexandreferreirajp01@gmail.com>
2025-12-07 19:16:45 +0000

    File changes

--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -628,7 +628,7 @@ export default function Jobs() {
                     <div className="flex items-start justify-between">
                       <Link to={createPageUrl('JobDetail') + `?id=${job.id}`} className="flex-1">
                         <JobCardContent job={job} viewCount={viewCount} />
-                      </div>
+                      </Link>
                       <div className="flex flex-col gap-1 ml-3">
                         {user && (
                           <Button
