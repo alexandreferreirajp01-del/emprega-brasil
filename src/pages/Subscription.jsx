91f3d21ee20ba4d35e4068c44d3f59a17165ae92
Alexandre Ferreira <alexandreferreirajp01@gmail.com>
2025-12-13 15:39:20 +0000

    File changes

--- src/pages/Subscription.jsx
+++ src/pages/Subscription.jsx
@@ -23,7 +23,7 @@ export default function Subscription() {
   }, []);
 
   const handleBasic = () => {
-    window.location.href = 'https://vagasabertasparaiba.info/splash';
+    window.location.replace('https://vagasabertasparaiba.info/splash');
   };
 
   const handlePremium = () => {
