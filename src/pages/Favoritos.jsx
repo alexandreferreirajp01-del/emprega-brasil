9437b5eedf536d18d5768259d6a0a866da3519f6
Alexandre Ferreira <alexandreferreirajp01@gmail.com>
2025-12-13 02:40:42 +0000

    File changes

--- src/pages/Favoritos.jsx
+++ src/pages/Favoritos.jsx
@@ -7,6 +7,7 @@ import { Link } from "react-router-dom";
 import { createPageUrl } from "@/utils";
 import { base44 } from "@/api/base44Client";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import VisitorRedirect from "@/components/common/VisitorRedirect";
 
 export default function Favoritos() {
   const [user, setUser] = useState(null);
