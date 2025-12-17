089619c4219c7989f9eb75ca484f39caac22b1b3
Alexandre Ferreira <alexandreferreirajp01@gmail.com>
2025-12-17 15:12:34 +0000

    File changes

--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -289,9 +289,9 @@ export default function Jobs() {
       setIsLoading(true);
       
       const [jobsData, viewsData, favoritesData] = await Promise.all([
-        safeFetch(() => base44.entities.Job.list('-created_at', 500)),
-        safeFetch(() => base44.entities.JobView.list('-created_at', 2000)),
-        user ? safeFetch(() => base44.entities.FavoriteJob.list('-created_at', 500)) : Promise.resolve([])
+        safeFetch(() => base44.entities.Job.list('-created_date', 500)),
+        safeFetch(() => base44.entities.JobView.list('-created_date', 2000)),
+        user ? safeFetch(() => base44.entities.FavoriteJob.list('-created_date', 500)) : Promise.resolve([])
       ]);
 
       if (mounted) {
